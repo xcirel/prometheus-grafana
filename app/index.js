fe3ee8e14e572e44c2194ae12d91903d54e42b51
Eric dos Santos <eubdsantos@gmail.com>
2023-04-29 09:11:08 -0300

    Added new metrics

--- app/index.js
+++ app/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var prom = require('prom-client');
+const register = prom.register;
 
 var app = express();
 
+const counter = new prom.Counter({
+    name: 'class_request_total',
+    help: 'Requests counter',
+    labelNames: ['statusCode']
+});
+
+const gauge = new prom.Gauge({
+    name: 'class_free_bytes',
+    help: 'Example of gauge'
+});
+
+
 app.get('/', (req, res, next) => {
+  counter.labels('200').inc();
+  counter.labels('300').inc();
+  gauge.set(100*Math.random());
   res.send('Hello World');
+});
+
+app.get('/metrics', async function(req, res) {
+    res.set('Content-Type', register.ContentType);
+    res.end(await register.metrics());
 });
 
 app.listen(3000);
